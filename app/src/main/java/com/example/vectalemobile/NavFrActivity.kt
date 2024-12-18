912e402850fbc804ddad4e0e3f6a82f23a5b9988
abnderrazak-errifaouy <abdrzakrifawi@gmail.com>
2024-12-18 16:54:12 +0100

    add naveFr

---
+++ app/src/main/java/com/example/vectalemobile/NavFrActivity.kt
@@ -0,0 +1,17 @@
+package com.example.vectalemobile
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class NavFrActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nav_fr)
+
+    }
+
+}
