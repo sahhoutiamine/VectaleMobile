20d1c7157df03b8b436017decdf292721faba28a
abnderrazak-errifaouy <abdrzakrifawi@gmail.com>
2024-12-20 17:43:43 +0100

    add main p age

--- app/src/main/java/com/example/vectalemobile/MainPage.kt
+++ app/src/main/java/com/example/vectalemobile/MainPage.kt
@@ -1,12 +1,17 @@
 package com.example.vectalemobile
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.vectalemobile.classes.data.Lists
+import java.time.LocalDateTime
 
 class MainPage : AppCompatActivity() {
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_page_activity)
@@ -17,5 +22,24 @@ class MainPage : AppCompatActivity() {
         val last_name = findViewById<TextView>(R.id.last_name)
         val codeCard = findViewById<TextView>(R.id.code_number)
         val imgUser = findViewById<ImageView>(R.id.user_icon)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+
+        first_name.text = usr.nom
+        last_name.text = usr.prenom
+        for ( i in Lists().listPacks){
+            if (i.idUser == usr.id){
+                codeCard.text = i.code
+                for (j in Lists().listCards){
+                    Toast.makeText(this,j.code,Toast.LENGTH_SHORT).show()
+                    if (j.active && j.code == i.code){
+                        progressBar.progress = LocalDateTime.now().dayOfMonth
+                    }
+
+            }
+        }
+
+
     }
+
 }
+    }
--- app/src/main/java/com/example/vectalemobile/classes/data/Lists.kt
+++ app/src/main/java/com/example/vectalemobile/classes/data/Lists.kt
@@ -1,13 +1,17 @@
 package com.example.vectalemobile.classes.data
 
 import com.example.vectalemobile.R
+import com.example.vectalemobile.classes.FunctionsClass
 import com.example.vectalemobile.classes.info.Card
 import com.example.vectalemobile.classes.info.Pack
+import com.example.vectalemobile.classes.info.PackType
 import com.example.vectalemobile.classes.info.User
 
 class Lists {
+
     val listUsers = mutableListOf<User>(User(0,"a","a","a","a",
         "a","a","a","amine@gmail.com","sahhouti", R.drawable.id_card_ic))
-    val listPacks = mutableListOf<Pack>()
-    val listCards = mutableListOf<Card>()
+    var cc ="aaa"
+    val listPacks = mutableListOf<Pack>(Pack(cc, 0 , PackType.Vip))
+    val listCards = mutableListOf<Card>(Card(0 , listPacks[0].code ,true))
 }
